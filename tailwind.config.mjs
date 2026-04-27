c3b55b215ae60667e5a10e2f2152498bc7e00bf0
csucclsgorgdbebs <csucclsgorgdb@gmail.com>
2026-04-27 11:40:00 +0800

    Update and rename tailwind.config.js to tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,9 +3,10 @@ export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./context/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
